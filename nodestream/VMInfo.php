a325e9ac543a467adc1943a43060020c4408e421
MitchellSParker <mparker95@yahoo.com>
2019-04-15 22:53:24 -0400

    Update VMInfo.php

--- nodestream/VMInfo.php
+++ nodestream/VMInfo.php
@@ -1,6 +1,6 @@
 <?php
 $host = ""; 		//This VMs Internal IP can be acquired by running "hostname -I"
-$port = 8002;						//This VMs Port
+$port = 8001;						//This VMs Port
 $DC = "104.196.147.215";	//DataCenter IP
-$DCport = 8001;					//DataCenter Port
+$DCport = 8002;					//DataCenter Port
 ?>
